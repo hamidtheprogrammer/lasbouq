ff6f738510c430d84a1d2be40a5667a408549ec3
Owoyemi Abdulhameed <owoyemiabdulhameed@mac.home>
2026-02-06 15:36:02 +0000

    added location section-- plan to change the UI for this section in future

--- client/app/(pages)/home/Location.tsx
+++ client/app/(pages)/home/Location.tsx
@@ -5,16 +5,16 @@ import Link from "next/link";
 const Location = () => {
   return (
     <>
-      <section className="px-5 mt-34 max-sm:mt-20">
+      <section className="px-5 mt-34 md:mb-27 max-sm:mt-20">
         <div className="mb-12 text-foreground flex max-md:flex-col justify-between items-center space-y-6">
           <div className="max-md:text-center space-y-1">
             <p className="text-xs tracking-[0.3rem]">OUR LOCATIONS</p>
-            <h1 className="font-italiana text-6xl max-sm:text-4xl md:max-w-[30rem] text-balance">
+            <h1 className="font-italiana text-5xl max-sm:text-4xl md:max-w-[30rem] text-balance">
               Platey Ipsum vendeur corea
             </h1>
           </div>
           <Link
-            className={`h-fit group border border-foreground hover:border-none relative ${styles} before:bg-foreground`}
+            className={`h-fit group border border-foreground relative ${styles} before:bg-foreground`}
             href={"/apply"}
           >
             <p
@@ -24,7 +24,7 @@ const Location = () => {
             </p>
           </Link>
         </div>
-        <ul className="flex flex-wrap gap-5 max-sm:gap-14">
+        <ul className="flex flex-wrap gap-20 max-sm:gap-14">
           {Array.from({ length: 3 }).map((_, idx) => (
             <li
               key={idx}
@@ -35,7 +35,7 @@ const Location = () => {
                 width={400}
                 height={400}
                 alt="location-image"
-                className="w-full aspect-square"
+                className="w-full aspect-video"
               />
               <div className="space-y-2">
                 <h1 className="font-semibold text-xl">Peter Taylor house</h1>
@@ -50,17 +50,24 @@ const Location = () => {
               </Link>
             </li>
           ))}
-          ß
+      
         </ul>
       </section>
-      <section className="relative h-full before:content-[''] before:relative before:z-10 before:h-20 before:w-full before:rounded-b-2xl before:bg-background">
+      <section className="relative h-screen">
+        <div className="relative z-10 h-20 w-full rounded-b-2xl bg-background">
+      
+        </div>
         <Image
-          className="absolute inset-0 w-full object-cover"
+          className="absolute inset-0 size-full object-cover"
           src={"/hero.png"}
           alt="image"
           width={1000}
           height={1000}
         />
+        <div className="inset-0 size-full absolute bg-black/40"></div>
+        <div className="absolute bottom-0 left-0 z-10 h-20 w-full rounded-t-2xl bg-background">
+      
+        </div>
       </section>
     </>
   );
